d57d3b0e6b0a7c21c8d61884e4209609f1dc2075
Pjlouredo <pjlouredo@gmail.com>
2024-11-21 17:30:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,44 +14,65 @@ document.querySelectorAll(".link-menu").forEach((anchor) => {
   });
 });
 
-// Controle do modal
-const modal = document.getElementById("videoModal");
-const link = document.getElementById("sobre-nos-link");
-const close = document.querySelector(".close");
+// Controle do modal de vídeo
+const videoModal = document.getElementById("videoModal");
+const linkVideo = document.getElementById("sobre-nos-link");
+const closeVideoModal = document.querySelector("#videoModal .close");
 
-// Abrir o modal ao clicar no link e iniciar reprodução do vídeo
-link.onclick = function (e) {
+// Abrir o modal de vídeo
+linkVideo.onclick = function (e) {
   e.preventDefault();
-  modal.style.display = "block";
+  videoModal.style.display = "block";
   playVideo();
 };
 
-// Fechar o modal ao clicar no botão "close"
-close.onclick = function () {
-  modal.style.display = "none";
+// Fechar o modal de vídeo
+closeVideoModal.onclick = function () {
+  videoModal.style.display = "none";
   pauseVideo();
 };
 
-// Fechar o modal ao clicar fora dele
+// Fechar modal ao clicar fora dele
 window.onclick = function (event) {
-  if (event.target === modal) {
-    modal.style.display = "none";
+  if (event.target === videoModal) {
+    videoModal.style.display = "none";
     pauseVideo();
   }
 };
 
-// Função para reproduzir o vídeo
+// Funções para controlar o vídeo
 function playVideo() {
-  const video = modal.querySelector("video");
-  if (video) {
-    video.play();
-  }
+  const video = videoModal.querySelector("video");
+  if (video) video.play();
 }
 
-// Função para pausar o vídeo
 function pauseVideo() {
-  const video = modal.querySelector("video");
-  if (video) {
-    video.pause();
-  }
+  const video = videoModal.querySelector("video");
+  if (video) video.pause();
 }
+
+// Controle do modal de descrição dos produtos
+const productModal = document.getElementById("productModal");
+const modalDescription = document.getElementById("modal-description");
+const closeProductModal = document.querySelector("#productModal .close");
+
+// Abrir o modal com descrição de produto
+document.querySelectorAll(".info-btn").forEach((button) => {
+  button.addEventListener("click", () => {
+    const description = button.getAttribute("data-info");
+    modalDescription.innerHTML = description;
+    productModal.style.display = "block";
+  });
+});
+
+// Fechar o modal de produtos
+closeProductModal.addEventListener("click", () => {
+  productModal.style.display = "none";
+});
+
+// Fechar modal de produtos ao clicar fora
+window.addEventListener("click", (event) => {
+  if (event.target === productModal) {
+    productModal.style.display = "none";
+  }
+});
